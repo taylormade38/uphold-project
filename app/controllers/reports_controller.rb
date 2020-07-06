e22e61a7578b3b0eb77c7cd176c294ed87f4d955
Taylor Fong <taylor.fong.2012@gmail.com>
2020-07-07 09:49:37 +1000

    filled in methods for reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,49 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: [:show, :edit, :update, :destroy]
+
   def new
+    @report = Report.new
+    authorize @report
   end
 
   def create
+    @report = Report.new(report_params)
+    authorize @report
+    @report.user = current_user
+    if @report.save
+      redirect_to report_path(@report)
+    else
+      render :new
+    end
   end
 
   def show
+    authorize @report
   end
 
   def edit
+    authorize @report
   end
 
   def update
+    authorize @report
+    @report.update(report_params)
+    redirect_to report_path(@report)
   end
 
   def destroy
+    authorize @report
+    @report.destroy
+    redirect_to city_path(@city)
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:city, :content, :evaluation)
   end
 end
