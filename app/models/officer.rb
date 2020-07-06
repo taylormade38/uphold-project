1b8c946e1f5461e9535e4e954a673174f0838d1b
Taylor Fong <taylor.fong.2012@gmail.com>
2020-07-06 18:00:43 +1000

    added validations to models

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -1,3 +1,4 @@
 class Officer < ApplicationRecord
   belongs_to :city
+  has_many :reports
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,13 @@
 class Report < ApplicationRecord
+  EVALUATION = ['positive', 'negative', 'neutral']
+
   belongs_to :user
   belongs_to :officer
   belongs_to :city
+  has_many :report_tags, dependent: :destroy
+  has_many :tags, through: :report_tags
+
+  validates :city, presence: true
+  validates :content, presence: true
+  validates :evaluation, inclusion: { in: EVALUATION }
 end
