3c9b94f72147c91c15a56f4df9102cafbf4eed3d
Harold Seah <harryseahjy@gmail.com>
2020-07-07 12:33:19 +0800

    Fixed officer model validations and associations

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -2,7 +2,8 @@ class Officer < ApplicationRecord
   belongs_to :city
   has_many :reports
 
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :badge_number, presence: true
   validates :department, presence: true
 end
