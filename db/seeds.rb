27cc105f0a05078d25810450072119e595dbd011
Harold Seah <harryseahjy@gmail.com>
2020-07-08 21:13:52 +0800

    Formatted show page cards, title, and write report button

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,50 +45,54 @@
 
 # # HAROLD's CODE
 
-officers = Officer.all
+# officers = Officer.all
 
-officers.each do |officer|
+# officers.each do |officer|
 
-    10.times do
-        report = Report.new
-        report.content = Faker::Lorem.sentence(word_count: 12)
-        report.address = Faker::Address.full_address
-        report.officer = officer
-        report.user = User.first
-        report.city = City.all.sample
-        report.evaluation = %w(positive negative neutral).sample
-        report.save!
-    end
+#     10.times do
+#         report = Report.new
+#         report.content = Faker::Lorem.sentence(word_count: 12)
+#         report.address = Faker::Address.full_address
+#         report.officer = officer
+#         report.user = User.first
+#         report.city = City.all.sample
+#         report.evaluation = %w(positive negative neutral).sample
+#         report.save!
+#     end
 
-end
-
-
-puts "Creating tags..."
-
-# tags = %w(Physical-Assault Weapon-Used Racist-Remark Racially-Profiled Excessive-Force Verbal-Abuse Chokehold)
-
-tag1 = {name: 'Weapon Used', info: 'The officer used a weapon.'}
-tag2 = {name: 'Physical Assault', info: 'The officer physically assaulted me.'}
-tag3 = {name: 'Weapon Used', info: 'The officer used a weapon against me, like a taser, baton, pepper spray, gun, etc.'}
-tag4 = {name: 'Racially Profiled', info: 'The officer only stopped me because of my skin color. I was doing nothing wrong.'}
-tag5 = {name: 'Excessive Force', info: 'The officer used an unnecessary amount of force to hold me.'}
-tag6 = {name: 'Community Involvement', info: 'The officer is engaged in our community.'}
-tag7 = {name: 'Education', info: 'The officer taught participated in an event where he taught the community something new.'}
-tag8 = {name: 'Chokehold', info: 'The officer held me in a chokehold'}
-
-[ tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8 ].each do |attributes|
-  tag = Tag.create!(attributes)
-  puts "Created #{tag.name}"
-end
-
-# 500.times do
-#   report_tag = ReportTag.new
-#   report_tag.report = Report.all.sample
-#   report_tag.tag = Tag.all.sample
-#   report_tag.save!
 # end
 
-# puts "Finished!"
+
+# puts "Creating tags..."
+
+# # tags = %w(Physical-Assault Weapon-Used Racist-Remark Racially-Profiled Excessive-Force Verbal-Abuse Chokehold)
+
+# tag1 = {name: 'Weapon Used', info: 'The officer used a weapon.'}
+# tag2 = {name: 'Physical Assault', info: 'The officer physically assaulted me.'}
+# tag3 = {name: 'Weapon Used', info: 'The officer used a weapon against me, like a taser, baton, pepper spray, gun, etc.'}
+# tag4 = {name: 'Racially Profiled', info: 'The officer only stopped me because of my skin color. I was doing nothing wrong.'}
+# tag5 = {name: 'Excessive Force', info: 'The officer used an unnecessary amount of force to hold me.'}
+# tag6 = {name: 'Community Involvement', info: 'The officer is engaged in our community.'}
+# tag7 = {name: 'Education', info: 'The officer taught participated in an event where he taught the community something new.'}
+# tag8 = {name: 'Chokehold', info: 'The officer held me in a chokehold'}
+
+# [ tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8 ].each do |attributes|
+#   tag = Tag.create!(attributes)
+#   puts "Created #{tag.name}"
+# end
+
+reports = Report.all
+
+reports.each do |report|
+    5.times do
+      report_tag = ReportTag.new
+      report_tag.report = report
+      report_tag.tag = Tag.all.sample
+      report_tag.save!
+    end
+end
+
+
 
 
 # cities = City.all
