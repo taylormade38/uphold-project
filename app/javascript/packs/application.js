b70a4e2e39fc9506a0b86a1b69c14bd8ae848966
Taylor Fong <taylor.fong.2012@gmail.com>
2020-07-08 16:55:15 +1000

    enabed toggle active class on tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,8 @@ require("channels")
 // External imports
 import "bootstrap";
 import "../plugins/popover_activation";
+import "../plugins/tag_choice";
+
 
 import { autocomplete } from "../components/autocomplete"
 import { initMap } from './map'
