0565967e9c7e6c48e926277ace755027a6db9c13
Alexander <alexander@Alex.local>
2020-07-09 16:04:06 +0700

    added layout for dash

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,40 @@
 # #
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
-Officer.destroy_all
-City.destroy_all
+# puts "Cleaning database..."
+# Officer.destroy_all
+# City.destroy_all
 
-5.times do
-  User.create!(email: Faker::Internet.email, password: 'password')
-end
+# 5.times do
+#   User.create!(email: Faker::Internet.email, password: 'password')
+# end
 
-puts "Creating cities..."
-city1 = {name: 'New York City', state: 'New York'}
-city2 = {name: 'Los Angeles', state: 'California'}
-city3 = {name: 'Chicago', state: 'Illinois'}
-city4 = {name: 'Houston', state: 'Texas'}
-city5 = {name: 'Phoenix', state: 'Arizona'}
-city6 = {name: 'Philadelphia', state: 'Pennsylvania'}
-city7 = {name: 'San Antonio', state: 'Texas'}
-city8 = {name: 'San Diego', state: 'California'}
+# puts "Creating cities..."
+# city1 = {name: 'New York City', state: 'New York'}
+# city2 = {name: 'Los Angeles', state: 'California'}
+# city3 = {name: 'Chicago', state: 'Illinois'}
+# city4 = {name: 'Houston', state: 'Texas'}
+# city5 = {name: 'Phoenix', state: 'Arizona'}
+# city6 = {name: 'Philadelphia', state: 'Pennsylvania'}
+# city7 = {name: 'San Antonio', state: 'Texas'}
+# city8 = {name: 'San Diego', state: 'California'}
 
-[ city1, city2, city3, city4, city5, city6, city7, city8 ].each do |attributes|
-  city = City.create!(attributes)
-  puts "Created #{city.name}"
-end
-puts "Finished!"
+# [ city1, city2, city3, city4, city5, city6, city7, city8 ].each do |attributes|
+#   city = City.create!(attributes)
+#   puts "Created #{city.name}"
+# end
+# puts "Finished!"
 
-puts "Creating officers .."
-32.times do
-    Officer.create(
-        first_name: Faker::Name.male_first_name,
-        last_name: Faker::Name.last_name,
-        badge_number: Faker::IDNumber.spanish_citizen_number,
-        department: Faker::Address.community,
-        city: City.all.sample
-      )
-end
+# puts "Creating officers .."
+# 32.times do
+#     Officer.create(
+#         first_name: Faker::Name.male_first_name,
+#         last_name: Faker::Name.last_name,
+#         badge_number: Faker::IDNumber.spanish_citizen_number,
+#         department: Faker::Address.community,
+#         city: City.all.sample
+#       )
+# end
 
 
 
@@ -64,32 +64,32 @@ officers.each do |officer|
 end
 
 
-puts "Creating tags..."
+# puts "Creating tags..."
 
-tags = %w(Physical-Assault Weapon-Used Racist-Remark Racially-Profiled Excessive-Force Verbal-Abuse Chokehold)
+# tags = %w(Physical-Assault Weapon-Used Racist-Remark Racially-Profiled Excessive-Force Verbal-Abuse Chokehold)
 
-tag1 = {name: 'Weapon Used', info: 'The officer used a weapon.'}
-tag2 = {name: 'Physical Assault', info: 'The officer physically assaulted me.'}
-tag3 = {name: 'Weapon Used', info: 'The officer used a weapon against me, like a taser, baton, pepper spray, gun, etc.'}
-tag4 = {name: 'Racially Profiled', info: 'The officer only stopped me because of my skin color. I was doing nothing wrong.'}
-tag5 = {name: 'Excessive Force', info: 'The officer used an unnecessary amount of force to hold me.'}
-tag6 = {name: 'Community Involvement', info: 'The officer is engaged in our community.'}
-tag7 = {name: 'Education', info: 'The officer taught participated in an event where he taught the community something new.'}
-tag8 = {name: 'Chokehold', info: 'The officer held me in a chokehold'}
+# tag1 = {name: 'Weapon Used', info: 'The officer used a weapon.'}
+# tag2 = {name: 'Physical Assault', info: 'The officer physically assaulted me.'}
+# tag3 = {name: 'Weapon Used', info: 'The officer used a weapon against me, like a taser, baton, pepper spray, gun, etc.'}
+# tag4 = {name: 'Racially Profiled', info: 'The officer only stopped me because of my skin color. I was doing nothing wrong.'}
+# tag5 = {name: 'Excessive Force', info: 'The officer used an unnecessary amount of force to hold me.'}
+# tag6 = {name: 'Community Involvement', info: 'The officer is engaged in our community.'}
+# tag7 = {name: 'Education', info: 'The officer taught participated in an event where he taught the community something new.'}
+# tag8 = {name: 'Chokehold', info: 'The officer held me in a chokehold'}
 
-[ tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8 ].each do |attributes|
-  tag = Tag.create!(attributes)
-  puts "Created #{tag.name}"
-end
+# [ tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8 ].each do |attributes|
+#   tag = Tag.create!(attributes)
+#   puts "Created #{tag.name}"
+# end
 
-500.times do
-  report_tag = ReportTag.new
-  report_tag.report = Report.all.sample
-  report_tag.tag = Tag.all.sample
-  report_tag.save!
-end
+# 500.times do
+#   report_tag = ReportTag.new
+#   report_tag.report = Report.all.sample
+#   report_tag.tag = Tag.all.sample
+#   report_tag.save!
+# end
 
-puts "Finished!"
+# puts "Finished!"
 
 
 # cities = City.all
