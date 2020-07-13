904efd48af2e8dc0e2f3a2faf9c3973c797729fe
Alexander <alexander@Alex.local>
2020-07-13 13:37:34 +0700

    added toggler to footer

--- app/javascript/controllers/footer_controller.js
+++ app/javascript/controllers/footer_controller.js
@@ -1,14 +1,13 @@
 import { Controller } from "stimulus";
-
 export default class extends Controller {
   static targets = [ 'hide'];
   connect() {
     console.log(this.hideTarget);
   }
   hide(event) {
-    if this.hideTarget.style.display == "block";
-    this.hideTarget.style.display = "none";
-    else {
+    if (this.hideTarget.style.display == "block") {
+      this.hideTarget.style.display = "none"
+    } else {
       this.hideTarget.style.display = "block";
     }
   }
