22b0a806e12fc5f529ab552a245815b8f4112870
Harold Seah <harryseahjy@gmail.com>
2020-07-16 14:41:55 +0800

    Can now add new officer from new report, officer show page now has a map

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -4,10 +4,9 @@ import 'select2';
 const initSelect2 = () => {
     $(document).ready(function() {
         if (document.querySelector('#selectofficer')) {
-            console.log('hello')
             $('#selectofficer').select2({
-                data: JSON.parse(document.getElementById("selectofficer").dataset.officers)
-
+                data: JSON.parse(document.getElementById("selectofficer").dataset.officers),
+                tags: true
             });
         }
     });
