dd86a07db1a2edae51d695d1f29d87dd8399cd55
Keegan Noble <keegannoble90@gmail.com>
2020-07-16 14:25:45 +0800

    bookmark AJAX added

--- app/controllers/report_bookmarks_controller.rb
+++ app/controllers/report_bookmarks_controller.rb
@@ -4,7 +4,10 @@ class ReportBookmarksController < ApplicationController
     @report_bookmark = ReportBookmark.new(user: current_user, report: @report)
     authorize @report_bookmark
     @report_bookmark.save
-    redirect_to @report.city
+    respond_to do |format|
+      format.html { redirect_to @report.city }
+      format.js
+    end
   end
 
   def destroy
@@ -12,6 +15,9 @@ class ReportBookmarksController < ApplicationController
     @report_bookmark = ReportBookmark.where(user: current_user, report: @report).first
     authorize @report_bookmark
     @report_bookmark.destroy
-    redirect_to @report.city
+    respond_to do |format|
+      format.html { redirect_to @report.city }
+      format.js
+    end
   end
 end
